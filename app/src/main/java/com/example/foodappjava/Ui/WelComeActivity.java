e3378bf337c416e8da59a0db62777c772b0b2fe4
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-16 00:28:14 +0600

    dsd

--- app/src/main/java/com/example/foodappjava/Ui/WelComeActivity.java
+++ app/src/main/java/com/example/foodappjava/Ui/WelComeActivity.java
@@ -2,15 +2,29 @@ package com.example.foodappjava.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.foodappjava.R;
 
 public class WelComeActivity extends AppCompatActivity {
 
+
+    Button registration_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_wel_come);
+
+        registration_btn=findViewById(R.id.registration_btn);
+
+        registration_btn.setOnClickListener(view -> {
+            Intent intent=new Intent(getApplicationContext(),RegistrationActivity.class);
+            startActivity(intent);
+        });
     }
 }

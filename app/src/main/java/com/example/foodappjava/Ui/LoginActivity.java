445c28793592eed4d3983de24e612a66d9d673c7
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-17 23:36:40 +0600

    login and signup screen done

--- app/src/main/java/com/example/foodappjava/Ui/LoginActivity.java
+++ app/src/main/java/com/example/foodappjava/Ui/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.foodappjava.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.foodappjava.R;
 
@@ -12,5 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signUp(View view) {
+        startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+    }
+
+    public void loginUser(View view) {
+
+    }
+
+    public void forgotPassword(View view) {
+
     }
 }
--- app/src/main/java/com/example/foodappjava/Ui/RegistrationActivity.java
+++ app/src/main/java/com/example/foodappjava/Ui/RegistrationActivity.java
@@ -2,7 +2,9 @@ package com.example.foodappjava.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.foodappjava.R;
 
@@ -12,5 +14,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void userRegistration(View view) {
+
+    }
+
+    public void userSignIn(View view) {
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
     }
 }
